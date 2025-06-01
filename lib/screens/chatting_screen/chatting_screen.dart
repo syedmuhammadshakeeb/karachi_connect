e9bc3323e006b2740ddc725e115af5fb19660276
shakeebklab <136184688+syedmuhammadshakeeb@users.noreply.github.com>
2025-06-01 14:52:52 +0500

    new

--- lib/screens/chatting_screen/chatting_screen.dart
+++ lib/screens/chatting_screen/chatting_screen.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:karachi_connect/component/loading_image/loading_image.dart';
-import 'package:karachi_connect/component/profile_component/circular_profile_image.dart';
-import 'package:karachi_connect/component/text/custom_text.dart';
 import 'package:karachi_connect/screens/chatting_screen/component/chatting_screen_appbar.dart';
-import 'package:karachi_connect/utils/constants/colors.dart';
-import 'package:karachi_connect/utils/constants/icons.dart';
 import 'package:karachi_connect/utils/constants/images.dart';
-import 'package:karachi_connect/utils/styles/text_styles.dart';
 
 class ChattingScreen extends StatelessWidget {
   const ChattingScreen({super.key});
@@ -16,6 +10,7 @@ class ChattingScreen extends StatelessWidget {
     return Scaffold(
       appBar: const PreferredSize(
           preferredSize: Size.fromHeight(50), child: ChattingScreenAppbar()),
+      bottomNavigationBar: null,
       body: Container(
         width: MediaQuery.of(context).size.width,
         decoration: const BoxDecoration(
@@ -23,9 +18,7 @@ class ChattingScreen extends StatelessWidget {
               image: AssetImage(AppImages.bgImage), fit: BoxFit.cover),
         ),
         child: const Column(
-          children: [
-            
-          ],
+          children: [],
         ),
       ),
     );
---
+++ lib/screens/chatting_screen/component/chatting_screen_nav_bar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:karachi_connect/component/custom_text_field/custom_text_field.dart';
+
+class ChattingScreenNavBar extends StatelessWidget {
+  const ChattingScreenNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CustomTextField(),
+        
+      ],
+    );
+  }
+}
